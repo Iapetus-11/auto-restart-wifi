e18478c73b3b78e2cfcbc502d1494e42514e61c1
Milo Weinberg <iapetus011@gmail.com>
2026-01-22 07:42:05 -0500

    Fix systemd service setup and finish install script

--- install.sh
+++ install.sh
@@ -2,28 +2,55 @@
 
 set -e
 
+
 BINARY_INSTALL_DIR=/usr/local/bin
 BINARY_NAME=auto-restart-wifi
 BINARY_INSTALL_PATH="$BINARY_INSTALL_DIR/$BINARY_NAME"
 
-SYSTEMD_SERVICE_INSTALL_PATH=/etc/systemd/user/auto-restart-wifi.service
+SYSTEMD_SERVICE_INSTALL_PATH=/etc/systemd/system/auto-restart-wifi.service
+
+
+# Configure basic parameters (usb device, test address)
+
+echo 'USB devices: '
+lsusb
+echo
+
+read -rp 'Enter USB network device name: ' USB_DEVICE_TARGET </dev/tty
+
+echo
+read -rp 'Enter network test address (leave blank to use 1.1.1.1:80): ' TEST_ADDRESS </dev/tty
+
+if [[ -z $TEST_ADDRESS ]]; then
+    TEST_ADDRESS='1.1.1.1:80'
+fi
+
 
 # Download and install binary
+
 BINARY_DOWNLOAD_URL='https://github.com/{{GH_REPOSITORY}}/releases/download/{{GH_RELEASE_TAG}}/{{GH_RELEASE_BINARY}}'
-curl --output auto-restart-wifi "$BINARY_DOWNLOAD_URL"
+curl -sSL --output auto-restart-wifi "$BINARY_DOWNLOAD_URL"
+chmod +x "./$BINARY_NAME"
 mv "./$BINARY_NAME" "$BINARY_INSTALL_PATH"
 
+
 # Setup systemd service
+
 BINARY_INSTALL_PATH=$BINARY_INSTALL_PATH \
+USB_DEVICE_TARGET=$USB_DEVICE_TARGET \
+TEST_ADDRESS=$TEST_ADDRESS \
 envsubst << 'EOF' > $SYSTEMD_SERVICE_INSTALL_PATH
 [Unit]
 Description=Automatically restart USB network device when network connectivity is lost
-After=network.target
+After=network-online.target
 
 [Service]
-ExecStart=$BINARY_INSTALL_PATH
+ExecStart=$BINARY_INSTALL_PATH --test-address $TEST_ADDRESS --usb-device-target "$USB_DEVICE_TARGET"
 Type=simple
 
 [Install]
-default.targets
+WantedBy=multi-user.target
 EOF
+
+systemctl daemon-reload
+systemctl restart auto-restart-wifi
